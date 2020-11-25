4e1916e7bb76646ce8cd6aa0255aa9d4e7a9afe7
Leonel Puertas <leonel.puertas@incluit.com>
2020-11-25 15:11:55 -0300

    Agrego script tablas y proc

---
+++ SQL/script-usuarios.sql
@@ -0,0 +1,82 @@
+use das
+go
+-- drop table dbo.usuarios
+
+/* ---------------------------------------
+   Tabla: usuarios
+   --------------------------------------- */
+create table  dbo.usuarios
+(
+ nro_persona		integer				not null identity,
+ apellido			varchar(40)			not null,
+ nombre				varchar(255)		not null,
+ correo				varchar(255)		not null,
+ clave				varbinary(32)		not null,
+ token				uniqueidentifier	not null default newid(),
+ constraint PK__usuarios__END primary key(nro_persona),
+ constraint UK__usuarios__1__END unique(correo)
+);
+go
+
+declare @clave varchar(32) = '1',
+        @crypt varbinary(32) 
+
+set @crypt = hashbytes('sha1', @clave + replicate( '*', 32 - len(@clave)))
+
+insert into dbo.usuarios(apellido, nombre, correo, clave)
+values('PASTARINI', 'MARIELA', 'mpastarini@ubp.edu.ar', @crypt)
+go
+
+/* ---------------------------------------
+   Procedimiento: val_usuario
+   --------------------------------------- */
+create or alter procedure dbo.val_usuario
+(
+ @correo	varchar(255),
+ @clave		varchar(32),
+ @token		char(36)		output
+)
+as
+begin
+
+  declare @crypt varbinary(32) 
+
+  select @crypt  = hashbytes('sha1', @clave + replicate('*', 32 - len(@clave)))
+  
+  select @token = token
+    from dbo.usuarios (nolock)
+   where correo = @correo
+     and clave  = @crypt
+
+end
+go
+
+/*
+declare @clave	varchar(32) = '2',
+        @token	char(36)
+		
+execute dbo.val_usuario @correo='mpastarini@ubp.edu.ar', @clave=@clave, @token=@token output
+
+select @token
+*/
+
+/* ---------------------------------------
+   Procedimiento: get_datos_usuario
+   --------------------------------------- */
+create or alter procedure dbo.get_datos_usuario
+(
+ @token		char(36)
+)
+as
+begin
+
+  select apellido = upper(apellido),
+         nombre   = upper(nombre),
+		 correo   = lower(correo)
+	from dbo.usuarios (nolock)
+   where token = @token
+
+end
+go
+
+-- execute dbo.get_datos_usuario @token='7E3FD761-303E-4890-A38C-02DD97EAA04E'
